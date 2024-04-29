b30736e9ad99aa9429ea00dc4fd68e4d5bdf10f5
GuiAnjosP <163584133+GuiAnjosP@users.noreply.github.com>
2024-04-29 17:34:03 -0300

    Jogo Python
    Esse foi meu primeiro código em python publicado no GitHub. Acompanhe para ver a evolução.

---
+++ jogo.py
@@ -0,0 +1,9 @@
+print("Seja bem-vindo ao jogo, digite as palavras para tentar formar frases engraçadas")
+palavra1 = input("Digite uma palavra: ")
+palavra2 = input("Digite outra palavra: ")
+palavra3 = input("Digite outra palabra: ")
+
+print("O BRASIL PRECISA DE " ,palavra2, "E EU SOU MUITO " ,palavra1, "SOMOS TODOS" ,palavra3)
+
+nome = input("Digite seu nome!")
+print("Seja bem-vindo {}".format(nome))
